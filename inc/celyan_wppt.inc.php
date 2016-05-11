6f8c9d4c1a3494a4e761ad41229e1bae65154b57
yannbug <yann@abc.fr>
2016-05-11 15:37:34 +0200

    action quand plugin mis à jour

--- inc/celyan_wppt.inc.php
+++ inc/celyan_wppt.inc.php
@@ -8,8 +8,13 @@ class celyanWppt {
 	 */
 	public function __construct() {
 		
+		/** @see  wp-content/plugins/wppusher/Pusher/Handlers/UpdatePlugin.php:40 **/
+		add_action( 'wppusher_plugin_was_updated', array( $this, 'on_plugin_updated' ), 10, 1 );
 	}
 	
-	
+	public function on_plugin_updated( $wppusherPluginObj ) {
+		
+		var_dump( $wppusherPluginObj );
+	}
 }
 ?>
